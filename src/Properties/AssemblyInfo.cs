729c92bb23a701057d420178cdbd06d34a8c80ac
Mads Kristensen <post@madskristensen.net>
2016-04-15 10:05:29 -0700

    Add project files.

---
+++ src/Properties/AssemblyInfo.cs
@@ -0,0 +1,17 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Vsix Logger")]
+[assembly: AssemblyDescription("Logs exceptions and custom events to Application Insights")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Mads Kristensen")]
+[assembly: AssemblyProduct("Vsix Logger")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("en-US")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("f074f4e7-ef91-42f8-865b-d1ddb1d1c051")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
